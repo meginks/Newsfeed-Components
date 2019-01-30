5a39de58e894dea7b36b4d91c2a5e7e5efef1705
unknown <38929577+meginks@users.noreply.github.com>
2019-01-30 15:53:34 -0600

    created expanding button and made expanding articles functional

--- Article/Article.js
+++ Article/Article.js
@@ -5,7 +5,7 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.querySelectorAll(".expandButton");
+    this.expandButton = domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerHTML = "expand"; 
 
@@ -15,7 +15,7 @@ class Article {
   }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.expandButton.classList.toggle('article-open');
+    this.domElement.classList.toggle('article-open');
   }
 }
 
